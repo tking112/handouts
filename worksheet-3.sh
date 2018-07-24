18ec3cfc0b82c55bcc7ec7d2f13c6ee63f1771d1
tking112 <tking112@umd.edu>
2018-07-24 10:08:36 -0400

    Changed to UMD Address

---
+++ worksheet-3.sh
@@ -0,0 +1,10 @@
+# configure git
+
+git config --global user.name "tking112"
+git config --global user.email "tking112@umd.edu"
+git commit --no-edit --amend --reset-author
+
+# link your local repository to the origin repository on GitHub
+
+git remote add origin ...
+git push -u origin master
